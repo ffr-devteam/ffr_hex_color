5b9301de5eec05611b68e8c2ae09cf576dd36eed
Hanna Tonn <h.tonn@rafisa.ch>
2020-11-11 15:47:52 +0100

    formatted code

--- lib/ffr_hex_color.dart
+++ lib/ffr_hex_color.dart
@@ -3,8 +3,7 @@ library ffr_hex_color;
 import 'package:flutter/material.dart';
 
 class FFRHexColor extends Color {
-
-  //Assuming we have a valid hexColor string  
+  //Assuming we have a valid hexColor string
   static int _getColorFromHex(String hexColor) {
     hexColor = hexColor.toUpperCase().replaceAll("#", "");
     if (hexColor.length == 6) {
